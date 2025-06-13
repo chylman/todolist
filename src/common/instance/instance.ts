4b09620d0058b77eac5e83143c4dfbc13eee00d9
Ilja Afanasev <chylmanprosto@gmail.com>
2025-06-13 18:13:14 +0300

    feat: add data for requests in instance

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'https://api.example.com',
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
+    'API-KEY': import.meta.env.VITE_API_KEY,
+  },
 })
