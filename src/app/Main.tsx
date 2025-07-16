8002e8fc6477cb7cb49896fac9674ba5cc066e34
Ilja <chylmanprosto@gmail.com>
2025-07-16 11:00:40 +0300

    fix: fix bugs

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,7 +1,5 @@
 import { Navigate } from 'react-router'
 import { Container, Grid } from '@mui/material'
-import { useAppDispatch } from '@/common/hooks/useAppDispatch'
-import { createTodolist } from '@/features/todolists/model/todolistsSlice'
 import { CreateItemForm } from '@/common/components/AddItemForm/CreateItemForm'
 import { useAppSelector } from '@/common/hooks/useAppSelector.ts'
 import { selectIsLoggedIn } from '@/app/appSlice.ts'
@@ -10,13 +8,8 @@ import { useAddTodolistMutation } from '@/features/todolists/api/todolistsApi.ts
 import { Todolists } from '@/features/todolists/ui/todolists/Todolists'
 
 export const Main = () => {
-  const dispatch = useAppDispatch()
   const isLoggedIn = useAppSelector(selectIsLoggedIn)
   const [addTodolist] = useAddTodolistMutation()
-
-  const createTodolists = (title: string) => {
-    dispatch(createTodolist({ title }))
-  }
 
   if (!isLoggedIn) return <Navigate to={Path.Login} />
 
