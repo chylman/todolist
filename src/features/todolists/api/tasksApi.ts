f4e3494651d3a4ed5e7a84ae6170fbc3bb7dc917
Ilja <chylmanprosto@gmail.com>
2025-07-18 15:51:01 +0300

    feat: add createTask

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,12 +1,28 @@
 import { baseApi } from '@/app/baseApi.ts'
-import { GetTasksResponse } from '@/features/todolists/api/tasksApi.type.ts'
+import {
+  DomainTask,
+  GetTasksResponse,
+} from '@/features/todolists/api/tasksApi.type.ts'
+import { BaseResponse } from '@/common/types'
 
 export const taskApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getTasks: builder.query<GetTasksResponse, string>({
       query: (todolistId) => `/todo-lists/${todolistId}/tasks`,
+      providesTags: ['Task'],
+    }),
+    createTask: builder.mutation<
+      BaseResponse<DomainTask>,
+      { todolistId: string; title: string }
+    >({
+      query: ({ todolistId, title }) => ({
+        url: `/todo-lists/${todolistId}/tasks`,
+        method: 'POST',
+        body: { title },
+      }),
+      invalidatesTags: ['Task'],
     }),
   }),
 })
 
-export const { useGetTasksQuery } = taskApi
+export const { useGetTasksQuery, useCreateTaskMutation } = taskApi
