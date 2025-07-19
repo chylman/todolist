3f2cc3e8223ea70913f28ba0cdb608218c17219e
Ilja Afanasev <chylmanprosto@gmail.com>
2025-07-19 14:58:56 +0300

    fix: fix work to filter

--- src/features/todolists/ui/todolists/todolistsItem/FilterButtons/FilterButtons.tsx
+++ src/features/todolists/ui/todolists/todolistsItem/FilterButtons/FilterButtons.tsx
@@ -1,12 +1,9 @@
 import { Box, Button } from '@mui/material'
-import {
-  changeTodolistFilter,
-  changeTodolistFilterAC,
-  type FilterValuesType,
-} from '@/features/todolists/model/todolistsSlice'
-import { useDispatch } from 'react-redux'
+import { type FilterValuesType } from '@/features/todolists/model/todolistsSlice'
 import { deleteAllTasks } from '@/features/todolists/model/tasksSlice'
 import React from 'react'
+import { todolistsApi } from '@/features/todolists/api/todolistsApi'
+import { useAppDispatch } from '@/common/hooks/useAppDispatch'
 
 type Props = {
   activeFilter: FilterValuesType
@@ -17,9 +14,17 @@ export const FilterButtons: React.FC<Props> = ({
   activeFilter,
   todolistId,
 }) => {
-  const dispatch = useDispatch()
-  const changeTodoListFilterHandler = (newFilterValue: FilterValuesType) =>
-    dispatch(changeTodolistFilter({ filter: newFilterValue, id: todolistId }))
+  const dispatch = useAppDispatch()
+  const changeTodoListFilterHandler = (newFilterValue: FilterValuesType) => {
+    dispatch(
+      todolistsApi.util.updateQueryData('getTodolists', undefined, (state) => {
+        const todolist = state.find((todolist) => todolist.id === todolistId)
+        if (todolist) {
+          todolist.filter = newFilterValue
+        }
+      }),
+    )
+  }
 
   const deleteAllTasksHandler = (todolistId: string) => {
     dispatch(deleteAllTasks({ todolistId }))
--- src/features/todolists/ui/todolists/todolistsItem/TodolistItem.tsx
+++ src/features/todolists/ui/todolists/todolistsItem/TodolistItem.tsx
@@ -1,10 +1,10 @@
 import { CreateItemForm } from '@/common/components/AddItemForm/CreateItemForm'
 import React from 'react'
 import type { FilterValuesType } from '@/features/todolists/model/todolistsSlice'
-import { TodoListTitle } from './TodolistTitle/TodoListTitle'
-import { Tasks } from './Tasks/Tasks'
-import { FilterButtons } from '@/features/todolists/ui/Todolists/todolistsItem/FilterButtons/FilterButtons.tsx'
 import { useCreateTaskMutation } from '@/features/todolists/api/tasksApi.ts'
+import { Tasks } from '@/features/todolists/ui/todolists/todolistsItem/Tasks/Tasks'
+import { FilterButtons } from '@/features/todolists/ui/todolists/todolistsItem/FilterButtons/FilterButtons'
+import { TodoListTitle } from '@/features/todolists/ui/todolists/todolistsItem/TodolistTitle/TodoListTitle'
 
 type Props = {
   id: string
